af3af79897e238152153d3a9c56661af476826ab
Mayur Baravkar <mayurbaravkar24@gmail.com>
2024-01-07 18:47:39 +0530

    Hooks

--- React/{React]/Namaste-React/L1_L4/app.js
+++ React/{React]/Namaste-React/L1_L4/app.js
@@ -70,35 +70,3 @@ import ReactDOM from "react-dom/";
 // root.render(HeaderFunctionalComponent);
 
 
-
-const heading=React.createElement(
-  "h1",
-  {class:"Title"},
-  "This is a h1"
-);
-
-
-const heading2=React.createElement(
-  "h2",
-  {class:"Title"},
-  "This is a h2"
-);
-
-
-const heading3=React.createElement(
-  "h3",
-  {class:"Title"},
-  "This is a h3"
-);
-
-const container=React.createElement(
-  "div",
-  {Title:"container"},
-  [heading,heading2,heading3]
-);
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-//Rendering the react Element
-
-root.render(container);
